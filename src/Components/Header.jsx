264ce0cbcaf4b2fb4ab719ec31732ba382ddc44c
Werfmon <d.vyroubal.w@gmail.com>
2021-12-25 19:32:27 +0100

    main/header component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,135 +1,96 @@
-import React from "react";
+import React, {useEffect} from "react";
 import styled from "styled-components";
-import Button from "@mui/material/Button";
-import { fontSize } from "@mui/system";
+import "aos/dist/aos.css";
+import Aos from "aos";
+
 
 const HeaderComponent = styled.header`
   background-image: url("https://images.unsplash.com/photo-1554415707-6e8cfc93fe23?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80");
   height: 100vh;
   width: 100%;
-  box-shadow: inset 0 -5px 10px 0 #ffffff;
-  background-size: cover;
-  background-position: center;
-`;
-const Image = styled.div`
-  height: 100vh;
-  width: 100vw;
-  opacity: 0.93;
-  background-image: linear-gradient(205deg, #09adee, #8400ffdf);
-  background-repeat: no-repeat;
   background-size: cover;
   position: relative;
-  overflow-x: hidden;
+  top: 0;
+  z-index: -1;
 `;
-const H1 = styled.h1`
-  z-index: 20;
-  position: relative;
+const CoverHeader = styled.div`
+  background: linear-gradient(20deg, #6200ff, #0434e0cf);
+  height: 100%;
+  width: 100%;
+  display: grid;
+  place-content: center center;
+`;
+const Heading = styled.h1`
+  background-clip: text;
+  color: #ffffff;
+  /* background: linear-gradient(200deg, #222222d8, #150136dd); */
+  background-color: #00000032;
+  backdrop-filter: blur(3px);
+  border-bottom: 4px solid;
+  border-radius: 0 0 6px 6px;
+  padding: 4rem 6rem;
+  font-weight: 300;
+  font-size: clamp(4rem, 8vw, 20rem);
   font-family: "Kulim Park", sans-serif;
-  color: #ffffffe2;
-  font-size: clamp(1rem, 8vw, 8rem);
-`;
-const Span1 = styled.span`
-color: #ffffff;
-  margin-left: clamp(10rem, 45vw, 45rem);
-  `;
-const Span2 = styled.span`
-  color: #ffffffe2;
-  margin-left: clamp(5rem, 25vw, 25rem);
-`;
-const Icons = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: calc(100vh - 8rem);
-`;
-const HeaderBanner = styled.div`
-  width: 90vw;
-  height: 5rem;
-  display: flex;
-  justify-content: space-between;
-  position: absolute;
-  left: 50%;
-  transform: translateX(-50%);
-  &::after {
-    content: "";
-    width: 102%;
-    background-color: #fff;
-    height: 1px;
-    position: absolute;
-    left: 50%;
-    transform: translateX(-50%);
-    top: 90%;
-  }
-  @media screen and (max-width: 480px) and (min-width: 0) {
-    &{
-      flex-direction: column;
-    }
-    &::after{
-      top: 55%;
-    }
-  }
 `;
 const UnsortedList = styled.ul`
-  display: flex;
-  justify-content: space-evenly;
-  align-items: center;
+  position: absolute;
+  position: -webkit-sticky;
+  top: 0;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(8, 1fr);
+  background-color: #000000bd;
 `;
 const List = styled.li`
-  padding-inline: 10px;
-  cursor: pointer;
-  font-family: "Kulim Park", sans-serif;
-  text-align: center;
-  color: #ffffff;
-  font-size: 2.5rem;
-  font-weight: 100;
-`;
-const H2 = styled.h2`
-  font-family: "Kulim Park", sans-serif;
-  font-weight: 100;
+  padding-block: 0.5rem;
   display: flex;
+  width: max-content;
   align-items: center;
   justify-content: center;
-  color: #fff;
-  font-size: clamp(2rem, 2.8vw, 2.8rem);
+  &:nth-child(4) {
+    grid-column: 8 / 9;
+  }
+`;
+const ButtonHeader = styled.button`
+  padding: 0.5rem 2rem;
+  font-family: "Kulim Park", sans-serif;
+  cursor: pointer;
+  border: none;
+  color: #fffffffd;
+  font-size: 1.9rem;
+  font-weight: 300;
+  background-color: #24242800;
+  &:hover {
+    color: #ffffff60;
+  }
 `;
 
 export default function Header() {
-  const style = {
-    paddingInline: '10px',
-    paddingBottom: '0',
-    color: "#fff",
-    fontSize: 'clamp(1rem, 1.2vw, 2rem)',
-  }
+  useEffect(() => {
+      Aos.init({duration: 7000})
+  }, [])
   return (
-    <HeaderComponent>
-      <Image>
-        <HeaderBanner>
-          <H2>Profesio</H2>
-          <UnsortedList>
-            <List>
-              <Button style={style} href="#about">
-                About
-              </Button>
-            </List>
-            <List>
-              <Button style={style} href="#contact">
-                Contact
-              </Button>
-            </List>
-            <List>
-              <Button style={style} href="">
-                Login
-              </Button>
-            </List>
-          </UnsortedList>
-        </HeaderBanner>
-        <Icons>
-          <H1>
-            Čas na změnu! <br />
-            <Span2>Vyzkoušejte</Span2> <br /><Span1>Profesio</Span1>
-          </H1>
-        </Icons>
-      </Image>
-    </HeaderComponent>
+    <>
+      <HeaderComponent>
+        <CoverHeader>
+          <Heading data-aos='fade-up'>Profesio</Heading>
+        </CoverHeader>
+      </HeaderComponent>
+      <UnsortedList data-aos='fade-down'>
+        <List >
+          <ButtonHeader>O aplikaci</ButtonHeader>
+        </List>
+        <List >
+          <ButtonHeader>Nasazení</ButtonHeader>
+        </List >
+        <List>
+          <ButtonHeader>Kontakt</ButtonHeader>
+        </List >
+        <List>
+          <ButtonHeader>Login</ButtonHeader>
+        </List>
+      </UnsortedList>
+    </>
   );
 }
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,30 +1,183 @@
-import React from 'react'
-import styled from 'styled-components'
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import "aos/dist/aos.css";
+import Aos from "aos";
+import notebookAdmin from "../images/notebook_admin.png";
+import notebookTeacher from "../images/notebook_teacher.png";
+import notebookStudent from "../images/notebook_student.png";
 
 const MainComponent = styled.main`
-    min-height: 100vh;
-`
-const Container1 = styled.section`
-
-`
-const Container2 = styled.section`
-
-`
-const Container3 = styled.section`
-
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const Container = styled.div`
+  background-color: ${(props) => props.color};
+  width: 100%;
+  position: relative;
+  padding-bottom: 4rem;
+  &::before {
+    content: "";
+    top: 0;
+    position: absolute;
+    display: ${(props) => (props.visible ? "block" : "none")};
+    background-color: #0000ff89;
+    right: 0;
+    height: 2px;
+    width: ${props => props.len + '%'};
+  }
+`;
+const Heading = styled.h1`
+  color: #ffffff;
+  font-family: "Kulim Park", sans-serif;
+  font-weight: 200;
+  font-size: clamp(2rem, 4vw, 4rem);
+  text-align: center;
+  padding: 4rem 2rem;
+`;
+const Paragraph = styled.p`
+  color: #fff;
+  font-family: "Kulim Park", sans-serif;
+  font-weight: 200;
+  font-size: 2rem;
+  text-align: center;
+  padding: 1rem 2rem;
+  margin-top: 7rem;
+`;
+const Paragraph2 = styled.p`
+  font-family: "Kulim Park", sans-serif;
+  color: #ffffff;
+  text-align: center;
+  padding: 0rem 0rem 2rem 0;
+  font-size: 2rem;
+`;
+const Paragraph3 = styled.p`
+  font-family: "Kulim Park", sans-serif;
+  color: #ffffff;
+  font-weight: 200;
+  font-size: 1.5rem;
+  text-align: center;
+`;
+const NotebookContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-inline: 1rem;
+  margin-top: 4rem;
+`;
+const Img = styled.img`
+  margin-top: 1rem;
+`;
+const ImageContainer = styled.div`
+  border: 2px solid #027eaf;
+  border-radius: 50%;
+  background-color: #ffffff;
+  height: 22rem;
+  gap: 2rem;
+  width: 22rem;
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  & > p {
+    color: #027eaf;
+  }
+`;
+const UnList = styled.ul`
+  list-style: none;
+  width: 50%;
+  margin: 0 auto;
+`;
+const List = styled.li`
+  padding: 1rem 2rem;
+  margin: 1rem;
+  font-family: "Kulim Park", sans-serif;
+  color: #ffffff;
+  font-weight: 200;
+  font-size: 1.8rem;
+  display: flex;
+  align-items: center;
+  gap: 3rem;
+`;
+const Content = styled.article``;
+const Num = styled.div`
+  color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 5.5rem;
+  width: 5.5rem;
+  font-weight: 700;
+  font-size: 4rem;
+  border-radius: 0 50% 50% 50%;
+  background-color: #0000ff;
+`;
 export default function Main() {
-    return (
-        <MainComponent>
-            <Container1>
+  useEffect(() => {
+    Aos.init({
+      duration: 2000,
+    });
+  }, []);
 
-            </Container1>
-            <Container2>
-
-            </Container2>
-            <Container3>
-
-            </Container3>
-        </MainComponent>
-    )
+  return (
+    <MainComponent>
+      <Container color="#0a0a0a" >
+        <Heading>Tři různé pohledy!</Heading>
+        <NotebookContainer>
+          <ImageContainer data-aos="fade-right" data-aos-delay="100">
+            <Img src={notebookAdmin} alt="profesio application" />
+            <Paragraph2>Administrátor</Paragraph2>
+          </ImageContainer>
+          <ImageContainer data-aos="fade-down" data-aos-delay="100">
+            <Img src={notebookTeacher} alt="profesio application" />
+            <Paragraph2>Učitel</Paragraph2>
+          </ImageContainer>
+          <ImageContainer data-aos="fade-left" data-aos-delay="100">
+            <Img src={notebookStudent} alt="profesio application" />
+            <Paragraph2>Student</Paragraph2>
+          </ImageContainer>
+        </NotebookContainer>
+        <Content>
+          <Paragraph>
+            Naší aplikaci Profesio můžete používat jak se Vám zlíbí. <br />
+            <br /> Připravili jsme rozhraní pro učitele, studenty a
+            administrátory.
+          </Paragraph>
+        </Content>
+      </Container>
+      <Container color="#0a0a0a" visible len={60}>
+        <Heading>O Aplikaci</Heading>
+        <Paragraph3>
+          Profesio je open-source aplikace pro Vysoké Školy. <br />
+          <br />
+          Aplikace byla vytvořena pro vytváření rozvrhů pro studenty
+          <br />
+          Vysokých škol. <br />
+          <br />
+        </Paragraph3>
+      </Container>
+      <Container color="#0a0a0a" visible len={50}>
+        <Heading>Nasazení aplikace</Heading>
+        <UnList>
+          <List>
+            <Num>1</Num>Kontaktujete nás
+          </List>
+          <List>
+            <Num>2</Num>Domluvíme se
+          </List>
+          <List>
+            <Num>3</Num>Nasadíme naší aplikaci na server školy
+          </List>
+          <List>
+            <Num>4</Num>Administrátor nastaví potřebné informace
+          </List>
+          <List>
+            <Num>5</Num>Aplikace je připravená k použítí!
+          </List>
+        </UnList>
+      </Container>
+      <Container color="#0a0a0a" visible len={40}>
+        <Heading>Kontakt</Heading>
+        
+      </Container>
+    </MainComponent>
+  );
 }
